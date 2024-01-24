1d67618d4cce8289051617eed96375994d7b9861
Yann Klein <yann.klein@me.com>
2024-01-24 16:59:36 +0900

    fix typo and mistake on career journey user model

--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_24_050117) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.bigint "career_step_id"
     t.string "name"
     t.string "batch"
